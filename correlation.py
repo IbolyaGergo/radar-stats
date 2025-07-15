7005ca5b58dced8df8e609f21da8258d5a9f92d2
IbolyaGergo <ibolyagergi@gmail.com>
2025-07-15 15:08:00 +0200

    Working correlation.py without cli args.

--- correlation.py
+++ correlation.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+"""
+Plot correlation between adjacent rays for each elevation.
+"""
+
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
@@ -9,35 +13,27 @@ import wradlib as wrl
 import cmweather
 import sys
 
-if len(sys.argv) == 2:
-    sweep = "sweep_" + sys.argv[1]
-else:
-    sweep = "sweep_0"
-
 filename = "data/raw/odim_2024/vol_2024-10-11_00-05-00.h5"
 
-ds = xr.open_dataset(filename, group=sweep, engine="odim")
 dtree = xd.io.open_odim_datatree(filename)
 
 nsweep = len(dtree.sweep)
+
+
 fig, ax = plt.subplots()
-for i in range(nsweep):
-    sweep = f"sweep_{i}"
+for ns in range(nsweep):
+    sweep = f'sweep_{ns}'
     data = dtree[sweep]['TH']
     elev = data['elevation'].values[0]
-    print(sweep, flush=True)
- 
-    arr = xr.concat(
-        [
-            xr.corr(
-                data.isel(azimuth=i+1),
-                data.isel(azimuth=i)
-            )
-            for i in range (359)
-        ],
-        dim='azimuth_neighbor'
-    )
-    arr.plot(ylim=(0,1), add_legend=True, ax=ax, label=f'elev = {elev}')
+
+    print(f"Compute correlation for elevation {elev}", flush=True)
+
+    nazim = data['azimuth'].size
+    corrarr = np.empty( nazim - 1 )
+    for na in range( nazim - 1 ):
+        corrarr[na] = xr.corr( data.isel(azimuth=na+1),data.isel(azimuth=na) ) 
+    ax.plot(corrarr, label=f"elev = {elev}")
 ax.legend()
+ax.set(ylim = (0, 1))
 plt.show()
 # plt.savefig('results/correlation/corr_azim_dbzh_24', dpi=200)
