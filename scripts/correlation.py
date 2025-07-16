d6a1b95f88e427b7249577c0c2f3046b7ee355b9
IbolyaGergo <ibolyagergi@gmail.com>
2025-07-16 10:44:44 +0200

    Add threshold to correlation.py
    Now the correlation computation excludes vals that are below a given threshold.
    The threshold is computed from the min vals along a range ring plus a constant.

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -13,12 +13,18 @@ import wradlib as wrl
 import cmweather
 import sys
 
-filename = "data/raw/odim_2024/vol_2024-10-11_00-05-00.h5"
+filename = "../data/raw/odim_2024/vol_2024-10-11_00-05-00.h5"
 
 dtree = xd.io.open_odim_datatree(filename)
 
-nsweep = len(dtree.sweep)
+nsweep_max = len(dtree.sweep)
+if len(sys.argv) == 2:
+    nsweep = int(sys.argv[1])
+else:
+    nsweep = nsweep_max
 
+# 0 <= nsweep <= nsweep_max
+nsweep = np.amax( [0,  np.amin([nsweep, nsweep_max])] )
 
 fig, ax = plt.subplots()
 for ns in range(nsweep):
@@ -30,8 +36,15 @@ for ns in range(nsweep):
 
     nazim = data['azimuth'].size
     corrarr = np.empty( nazim - 1 )
+    threshold = data.min(dim='azimuth') + 30
     for na in range( nazim - 1 ):
-        corrarr[na] = xr.corr( data.isel(azimuth=na+1),data.isel(azimuth=na) ) 
+        ray_0 = data.isel(azimuth=na)
+        ray_1 = data.isel(azimuth=na+1)
+
+        corrarr[na] = xr.corr(
+            ray_0.where(ray_0 > threshold),
+            ray_1.where(ray_1 > threshold) 
+        ) 
     ax.plot(corrarr, label=f"elev = {elev}")
 ax.legend()
 ax.set(ylim = (0, 1))
