6623bbeb88ceadb1b36a6f2f879e9d2d6e2dceb5
IbolyaGergo <ibolyagergi@gmail.com>
2025-07-16 14:13:10 +0200

    Cli args now represent sweeps to plot

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -13,21 +13,14 @@ import wradlib as wrl
 import cmweather
 import sys
 
-filename = "../data/raw/odim_2024/vol_2024-10-11_00-05-00.h5"
+filename = "../data/raw/odim_2023/vol_2023-03-12_05-55-56.h5"
 
 dtree = xd.io.open_odim_datatree(filename)
 
-nsweep_max = len(dtree.sweep)
-if len(sys.argv) == 2:
-    nsweep = int(sys.argv[1])
-else:
-    nsweep = nsweep_max
-
-# 0 <= nsweep <= nsweep_max
-nsweep = np.amax( [0,  np.amin([nsweep, nsweep_max])] )
+sweep_nums = [ int(arg) for arg in sys.argv[1:] ]
 
 fig, ax = plt.subplots()
-for ns in range(nsweep):
+for ns in sweep_nums:
     sweep = f'sweep_{ns}'
     data = dtree[sweep]['TH']
     elev = data['elevation'].values[0]
@@ -36,17 +29,17 @@ for ns in range(nsweep):
 
     nazim = data['azimuth'].size
     corrarr = np.empty( nazim - 1 )
-    threshold = data.min(dim='azimuth') + 30
+    threshold = data.min(dim='azimuth') + 20
     for na in range( nazim - 1 ):
-        ray_0 = data.isel(azimuth=na)
-        ray_1 = data.isel(azimuth=na+1)
+        ray_0 = data.isel(azimuth = na)
+        ray_1 = data.isel(azimuth = na+1)
 
         corrarr[na] = xr.corr(
-            ray_0.where(ray_0 > threshold),
-            ray_1.where(ray_1 > threshold) 
+            ray_0.where(ray_0 >= threshold),
+            ray_1.where(ray_1 >= threshold) 
         ) 
     ax.plot(corrarr, label=f"elev = {elev}")
 ax.legend()
-ax.set(ylim = (0, 1))
+ax.set(ylim = (-1, 1))
 plt.show()
 # plt.savefig('results/correlation/corr_azim_dbzh_24', dpi=200)
