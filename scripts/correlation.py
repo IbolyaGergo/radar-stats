ab29bfca0576d4d0d804a32c626f02e75b58c916
IbolyaGergo <ibolyagergi@gmail.com>
2025-07-21 10:35:14 +0200

    Handle NaNs using np.masked arrays (10x speedup)
    Instead of xarray ufuncs isfinite, numpy masked arrays
    are used.
    The performance gain is around 10x.

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -13,6 +13,7 @@ from matplotlib import gridspec
 import xradar as xd
 import wradlib as wrl
 import cmweather
+import numpy.ma as ma
 
 PROJECT_ROOT = Path(__file__).parent.parent
 
@@ -28,7 +29,7 @@ fig, ax = plt.subplots()
 for nsweep in sweeps:
     if nsweep > max_sweep_idx:
         print(f"Max sweep index is {max_sweep_idx}")
-        break
+        exit(1)
 
     sweep = f'sweep_{nsweep}'
 
@@ -36,7 +37,7 @@ for nsweep in sweeps:
     noise_level_dB = da_TH.min(dim='azimuth')
     threshold_dB = 5
 
-    data_var = "TH"
+    data_var = "VRADV"
     data = dtree[sweep][data_var]
 
     elev = data['elevation'].values[0]
@@ -57,25 +58,34 @@ for nsweep in sweeps:
         mask_1 = da_TH.isel(azimuth = nazim+1) >= noise_level_dB + threshold_dB
         above_noise_1 = ray_1.where(mask_1)
 
+        # Dealing with NaNs
+        ma_above_noise_0 = ma.masked_invalid(above_noise_0.values)
+        ma_above_noise_1 = ma.masked_invalid(above_noise_1.values)
+    
+        corrarr[nazim] = ma.corrcoef(
+            ma_above_noise_0,
+            ma_above_noise_1
+        ).filled(np.nan)[0,1]
+
         # Check for sufficient number of valid data to compute correlation
         # Using ufuncs.isfinite insted of notnull:
         # notnull only deals with nan
         # ufuncs.isfinite deals also with +-inf (e.g. div by zero)
-        valid = xr.ufuncs.isfinite(above_noise_0) & xr.ufuncs.isfinite(above_noise_1)
-        n_valid = valid.sum()
+        # valid = xr.ufuncs.isfinite(above_noise_0) & xr.ufuncs.isfinite(above_noise_1)
+        # n_valid = valid.sum()
 
-        if n_valid >= 2: # required for xr.corr
-            corrarr[nazim] = xr.corr( above_noise_0, above_noise_1 ) 
-        else:
-            corrarr[nazim] = np.nan
+        # if n_valid >= 2: # required for xr.corr
+        #     corrarr[nazim] = xr.corr( above_noise_0, above_noise_1 ) 
+        # else:
+        #     corrarr[nazim] = np.nan
 
     ax.plot(corrarr, label=f"elev = {elev}")
 ax.legend()
 ax.set(ylim = (-1, 1))
 ax.set_title(f"threshold = noise + {threshold_dB}dB")
 
-# output_path = PROJECT_ROOT / "results" / "correlation" / (
-# filepath.stem + "_corr" + f"_{data_var}" + f"_thrs_{threshold_dB}" + ".png")
-# 
-# plt.savefig(output_path, dpi=200)
-# print(f"Created {output_path}")
+output_path = PROJECT_ROOT / "results" / "correlation" / (
+filepath.stem + "_corr" + f"_{data_var}" + f"_thrs_{threshold_dB}" + ".png")
+
+plt.savefig(output_path, dpi=200)
+print(f"Created {output_path}")
